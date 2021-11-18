e76f3f0308731df4ea8cc5bd22b8744ea938d7c5
ALazyGuy <razorprogrammer@gmail.com>
2021-11-18 16:19:32 +0300

    feat: Implemented filter for jwt

---
+++ web/src/main/java/com/epam/esm/filter/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.epam.esm.filter;
+
+import com.epam.esm.model.entity.UserEntity;
+import com.epam.esm.repository.UserRepository;
+import com.epam.esm.util.JwtUtils;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public JwtFilter(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String refreshToken = request.getHeader("refresh");
+        if (refreshToken != null) {
+            Claims claims = JwtUtils.getUsername(refreshToken);
+            String type = claims.get("type", String.class);
+            if (type != null && type.equals("refresh") && claims.getExpiration().after(new Date())) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(claims.getSubject());
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+    }
+}
