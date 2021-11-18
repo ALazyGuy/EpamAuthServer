02d959a309e6f3ad9148e8b5dd84197d1654cdf7
ALazyGuy <razorprogrammer@gmail.com>
2021-11-18 15:53:39 +0300

    feat: Implemented UserDetailsService

---
+++ service/src/main/java/com/epam/esm/model/dto/UserDetailsImpl.java
@@ -0,0 +1,61 @@
+package com.epam.esm.model.dto;
+
+import com.epam.esm.model.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> authorities;
+
+    public UserDetailsImpl(UserEntity user) {
+        this.username = user.getUsername();
+        this.password = user.getPasswordHash();
+        this.authorities = user.getRoles()
+                .stream()
+                .map(r -> new SimpleGrantedAuthority(r.getRole()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
